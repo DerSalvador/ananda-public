9acfc16a3e68beaeb352dd2e153af8dfee2a26e0
Michael Santos <msantana@swisscom.com>
2025-06-23 10:59:20 +0200

    merged

--- app/constants.py
+++ app/constants.py
@@ -49,12 +49,12 @@ DEFAULT_CONFIG ={
   },
   "Roi Offset": { 
     "description": "Will be added to the stoploss as resulting ROI, so if stoploss is -0.12 and roi_offset 0.02 will be added, ROI will be 0.14 in case of winrate high. In Case of winrate low, stoploss will be -0.1 to realite profit earlier",
-    "label": "8. ROI Offset (based on stoploss and winrate)",
+    "label": "8. ROI Offset (used to calculate ROI based on stoploss and winrate)",
     "value": 0.01
   },
   "Stoploss Offset": { 
     "description": "Will increase stoploss towards 0, in case of winrate high get out earlier. In Case of winrate low, decrease stoploss to allow more losses",
-    "label": "9. Stoploss Offset",
+    "label": "9. Stoploss Offset (adapt stoploss according to winrate)",
     "value": 0.005
   },  
   "GreedAndFearLimit": {
